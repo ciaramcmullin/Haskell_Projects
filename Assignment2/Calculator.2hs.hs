55724d28d6f30de9fd8d95ffeff55fe1d3206f18
Ciara McMullin <cbm389@nyu.edu>
2020-02-18 21:08:15 -0500

    Add files via upload

---
+++ Assignment2/Calculator.2hs.hs
@@ -0,0 +1,466 @@
+import Test.QuickCheck
+import Test.QuickCheck.Gen
+
+import Debug.Trace
+
+
+
+data BinOp = Plus | Minus | Times
+    deriving (Show, Eq)
+
+data Expr = Op BinOp Expr Expr -- Add BinOp before Expr Expr
+          | Lit Int
+    deriving (Show, Eq)
+
+-- Here are some handy utility functions for constructing example
+-- expressions.
+
+plus, minus, times :: Expr -> Expr -> Expr
+plus = Op Plus
+minus = Op Minus
+times = Op Times
+
+example :: Expr
+example = Op Times (Op Plus (Lit 10) (Lit 2)) (Lit 3)
+--example = (Op Plus (Lit 2) (Lit 0))
+--example = Lit 9
+
+interpBinOp :: BinOp -> (Int -> Int -> Int)
+interpBinOp Plus  = (+) -- This is Haskell notation to take an infix operator
+interpBinOp Minus = (-) -- and turn it into a normal non-infix "symbol"
+interpBinOp Times = (*)
+
+
+-- We can write an interpreter for these expressions, which takes an
+-- arithmetic expression and evaluates it to an integer.  (You might
+-- like to use interpBinOp; also, feel free to change the top-level
+-- declaration as you like, e.g. to add pattern matching on arguments.)
+
+interp :: Expr -> Int
+-- BEGIN interp (DO NOT DELETE THIS LINE)
+--interp (Lit a) = a
+--interp = undefined
+
+interp (Lit a) = a
+--interp (Op x y (Lit a)) = interpBinOp x interp y a
+--interp (Op x (Lit a) z) = interpBinOp x a interp z
+--interp (Op x (Lit a) (Lit b)) = interpBinOp x a b
+--interp (Op x y z) = interpBinOp x interp y interp z
+
+interp (Op Plus y z) = interp y + interp z
+interp (Op Minus y z) = interp y - interp z
+interp (Op Times y z) = interp y * interp z
+
+
+--interp = undefined
+-- END interp (DO NOT DELETE THIS LINE)
+
+-- Now we are going to write a simple optimizer for our expression
+-- language.  Technically, an optimizer is a bit useless for the
+-- particular language we have here, as we can always evaluate an
+-- expression to a number. However, it is not difficult to imagine a
+-- variant of our arithmetic language that has variables as well, in
+-- which case an optimizer would be useful.
+--
+-- We'll write a very simple optimizer which optimizes expressions of
+-- the form "e + 0" to "e".  To start off, write a PATTERN for this
+-- function which matches expressions which look like "e + 0" and
+-- optimizes them to "e". We DO NOT want you to attempt to optimized
+-- nested occurrences of this expression.
+--
+-- A real world peephole optimizer will include more algebraic
+-- identities than just e + 0. If you like, you can add some more;
+-- however, for the purposes of this lab, an implementation of
+-- simplifyZero that ONLY optimizes additions by zero is acceptable.
+
+simplifyZero :: Expr -> Expr
+-- BEGIN simplifyZero (DO NOT DELETE THIS LINE)
+simplifyZero (Op Plus e (Lit 0)) = e
+--simplifyZero (Op Plus e (Lit 0))  = e
+--simplifyZero e = e
+-- END simplifyZero (DO NOT DELETE THIS LINE)
+simplifyZero e = e
+
+-- No need to edit this now; you'll fix it later
+simplifier :: Expr -> Expr
+-- BEGIN simplifier (DO NOT DELETE THIS LINE)
+simplifier = simplifyZero
+-- END simplifier (DO NOT DELETE THIS LINE)
+
+
+
+
+
+-- You have probably been testing your programs correctness by running them
+-- on simple examples.  With QuickCheck, we can instead define
+-- properties that are programs should fulfill, and then test them
+-- on randomly generated inputs.
+--
+-- Here are three properties we might want our optimizer to uphold:
+--
+--      1. (Correctness) If we run the unoptimized and optimized
+--      versions, we should get the same result.
+--
+--      2. (Idempotency) Running the optimizer twice is equivalent to
+--      running it once.
+--
+--      3. (Optimality) After running the optimizer, there should be
+--      no more instances of the pattern that we looked for.
+--
+-- Please implement these three predicates using 'simplifier'.
+-- You can then test your program by writing 'check
+-- prop_optimizer_correctness' (or another property) in your GHCi
+-- session; you can also test the correctness of your properties by
+-- introducing bugs to the implementation and seeing if the properties
+-- fail.
+--
+-- Some of these properties will fail.  This is expected; read on!
+
+prop_optimizer_correctness :: Expr -> Bool
+-- BEGIN prop_optimizer_correctness (DO NOT DELETE THIS LINE)
+prop_optimizer_correctness x = interp x == interp (simplifier x) -- what is it trying to accomplish?
+--prop_optimizer_correctness = undefined
+-- END prop_optimizer_correctness (DO NOT DELETE THIS LINE)
+
+prop_optimizer_idempotent :: Expr -> Bool
+-- BEGIN prop_optimizer_idempotent (DO NOT DELETE THIS LINE)
+prop_optimizer_idempotent x = simplifier (simplifier x) == simplifier x
+-- END prop_optimizer_idempotent (DO NOT DELETE THIS LINE)
+-- For property (3), we've written the following helper function
+-- to look for instances where optimization can occur. 'findPlusZero'
+-- returns 'True' if there are any occurrences of e + 0 in an
+-- expression, and 'False' otherwise.  Please use it in your
+-- QuickCheck property.
+findPlusZero :: Expr -> Bool
+findPlusZero (Lit _)             = False
+findPlusZero (Op Plus e (Lit 0)) = True
+findPlusZero (Op _ e1 e2)        = findPlusZero e1 || findPlusZero e2
+
+prop_optimizer_optimizes :: Expr -> Bool
+-- BEGIN prop_optimizer_optimizes (DO NOT DELETE THIS LINE)
+prop_optimizer_optimizes (Lit _) = False
+prop_optimizer_optimizes x = findPlusZero (simplifier x)
+
+-- END prop_optimizer_optimizes (DO NOT DELETE THIS LINE)
+
+-- You may have noticed that property (3) is failing (if you
+-- are unlucky, property (2) may be failing too!), and QuickCheck
+-- is reporting quite a large randomly generated test input which
+-- induced the failure.  How should you go about debugging this problem?
+-- One approach is to copy paste the failing input into your GHCi
+-- session, and manually run your implementation with some tracing to
+-- try to figure out what's going on.
+--
+-- A better approach is to try to *minimize* the test case before trying
+-- to figure out what the bug is.  For example, if your test case is "Op
+-- Plus e1 e2", where e1 and e2 are some large expressions you might try
+-- testing on just e1.  Or you might try and shrink e1 to some smaller
+-- e1', and check if "Op Plus e1' e2" still exhibits the error.  So
+-- your workflow would be something like this:
+--
+-- To shrink a failing test case e:
+--  1. Generate some candidate smaller test case e' (by removing
+--     subexpressions, etc.)
+--  2. Test the property on e'
+--  3. If it still FAILS, recursively shrink e'.
+--     If it PASSES, go back to step 1 and generate a different candidate
+--     case.  If you run out of candidates, stop and return the smallest
+--     still failing test case.
+--
+-- QuickCheck has implemented this algorithm: all you have to do tell
+-- QuickCheck how to do step (1): given a failing test case
+-- e, generate a list of candidate smaller test cases.
+--
+-- In this file, we've told QuickCheck to use 'expr_shrink' to carry out
+-- step (1).  Implement 'expr_shrink', and test it by running 'check
+-- prop_optimizer_optimizes' and seeing if your test cases get smaller.
+-- (Hint: you may find a list comprehension useful here.)
+--
+-- If you can't figure out how to do this, feel free to skip ahead;
+-- it's not necessary for the rest of the lab.
+
+expr_shrink :: Expr -> [Expr]
+-- BEGIN expr_shrink (DO NOT DELETE THIS LINE)
+expr_shrink e = []
+-- END expr_shrink (DO NOT DELETE THIS LINE)
+
+-- At this point, it should be clear that the property fails
+-- because simplifyZero will only optimize an expression like 2 + 0, and
+-- not a more complex one like 2 + (2 + 0), since the addition of zero
+-- occurs in a nested expression; however, our property wants all nested
+-- occurrences to be simplified.  How should we fix this? We could
+-- rewrite this function to recursively look into subexpressions and
+-- attempt to optimize them too, but a better strategy is to define a
+-- HIGHER ORDER function which does this for us.
+--
+-- 'peephole' is a higher order function that takes an optimizer
+-- (like simplifyZero) and recursively applies it to every
+-- subexpression of an expression.  If 'simplifyZero' is an optimizer
+-- which only looks at the top-most expression, 'peephole simplifyZero'
+-- is an optimizer which looks at all sub-expressions.
+--
+-- (Hint: if your implementation of peephole infinite loops, try using
+-- the 'trace' function to get more insight into what is going on.)
+
+--peephole :: (Expr -> Expr) -> Expr -> Expr
+-- BEGIN peephole (DO NOT DELETE THIS LINE)
+
+--peephole f (Op _ y _) = peephole f y
+--peephole f (Op _ _ z) = peephole f z
+--peephole f (Op _ _ z) = f z
+-- END peephole (DO NOT DELETE THIS LINE)
+
+-- Once you are done, update your QuickCheck tests to use the
+-- peephole'ified optimizer, and verify that they are all passing.
+
+
+-- END peephole (DO NOT DELETE THIS LINE)
+
+-- Once you are done, update your QuickCheck tests to use the
+-- peephole'ified optimizer, and verify that they are all passing.
+
+-------------------------------------------------------------------
+
+--                  The instruction language
+
+-------------------------------------------------------------------
+
+-- Let's talk about the simple instruction language that you will be
+-- compiling your expressions to.  This language is defined by the
+-- following grammar:
+--
+--      b ::= + | - | *
+--      i ::= OP b | PUSH n
+--      is ::= i; is | {- empty -}          -- this is a list
+
+data Instr = IOp BinOp
+           | IPush Int
+    deriving (Show, Eq)
+
+-- What is the meaning of an instruction?  We are defining a stack
+-- machine, so instructions do one of two things:
+--
+--  1. They "PUSH" a literal value onto the stack, or
+--  2. They pop two values off the stack, do an "OP"eration on them,
+--  and then pushes the result onto the stack.  Note that this could
+--  FAIL if there are not enough values on the stack (a stack underflow
+--  error).
+--
+-- For example, consider the following program:
+--
+--      PUSH 2; PUSH 1; OP -; PUSH 3; OP *
+--
+-- After running the first two instructions, we have a stack consisting
+-- of [1, 2].  Now, we run the minus OP, which pops two elements
+-- off the stack and subtracts them.  In this lab, we will define the
+-- operation so that it subtracts the top value from the next value
+-- (this is an arbitrary choice; you can just as easily do it the
+-- other way; we choose this convention as it matches with the stack
+-- machines of many other well known runtimes like the JVM).
+-- This gives us 1, which we push back onto the stack, giving us a new
+-- stack [1].  Push 3 to get [3, 1], and then multiplication gives us a
+-- final stack of [3].
+--
+-- We'll represent stacks as just lists of integers, where the head
+-- of the list is the top of the stack.  In Haskell, lists are pure
+-- data structures which cannot be mutated; we will "mutate" the
+-- stack by returning a *new* stack from a function which represents
+-- the updated stack.
+
+type Stack = [Int]
+ex1::Maybe Stack
+ex1 = Just([1, 2])
+
+
+ex2::Maybe Stack
+ex2 = Just([4, 5])
+-- We can interpret an instruction as taking a stack, performing
+-- some operation on it, and returning the new stack after the
+-- operation, but possibly failing.  Please implement 'step', which
+-- does this.
+
+step :: Instr -> Stack -> Maybe Stack
+-- BEGIN step (DO NOT DELETE THIS LINE)
+--step (IPush y) [] = Nothing
+
+--step (IOp o) (x:_) = Nothing
+step (IPush x) xs =  Just (x:xs)
+step (IOp o) (x:y:ys) = Just (((interpBinOp o) y x):ys)-- implement POP if it has more than two elements
+    --where z = (interpBinOp o) y x
+--step (IOp o) (x:y:[]) = Just (((interpBinOp o) y x):[])
+    --where z = (interpBinOp o) y x
+step (IOp o) (x:_) = Nothing
+step (IOp o) [] = Nothing
+    --where z = (interpBinOp o) y x
+                    -- else, do Nothing
+--step = undefined
+-- END step (DO NOT DELETE THIS LINE)
+
+-- We should also tie this together, and write a function that
+-- takes a stream of instructions and an initial stack, and executes
+-- the instructions one by one until there are no more left.
+
+run :: [Instr] -> Stack -> Maybe Stack
+-- BEGIN run (DO NOT DELETE THIS LINE)
+run ([]) y = Nothing
+--run (x:y:xs) y = run xs hd  --(y:z:ys) How to concat Just(stack)? hd has Maybe Stack type
+  --  where hd = step x y
+-- END run (DO NOT DELETE THIS LINE)
+
+-------------------------------------------------------------------
+
+--                  A simple compiler
+
+-------------------------------------------------------------------
+
+-- Finally, we are ready to implement a compiler for our expression
+-- language.  This compiler takes an AST, and compiles it into a
+-- sequence of instructions which, when evaluated on an empty
+-- stack, results in a stack with one element that is equal to
+-- the result of interpreting the expression directly. (You'll get
+-- no points for cheekily emitting a single PUSH instruction!) Also,
+-- write a QuickCheck property to test this.
+--
+-- (Hint: This is a recursive function, and it can be implemented in
+-- two lines.)
+
+compile :: Expr -> [Instr]
+-- BEGIN compile (DO NOT DELETE THIS LINE)
+-- no points for cheekily emitting a single PUSH instruction
+compile (Lit a) = (IPush a):[]
+--compile (Op x y z) = (IOp x):[] ++ (compile y) ++ (compile z)   <===== Original version
+compile (Op x y z) = (compile y) ++ (IOp x):[] ++ (compile z)
+-- END compile (DO NOT DELETE THIS LINE)
+
+-- Your compiler is correct if running the instructions produced
+-- by the compiler on an empty stack gives you the same result as
+-- directly evaluating the expression.
+
+--prop_compile_correctness :: Expr -> Bool   -- Is definition complete?
+-- BEGIN prop_compile_correctness (DO NOT DELETE THIS LINE)
+--prop_compile_correctness e = run (compile e) ([]) == (interp e:[])
+-- END prop_compile_correctness (DO NOT DELETE THIS LINE)
+
+-- We've also provided for you a simple decompiler which takes
+-- a stream of instructions and translates it back into an expression
+-- that would generate it if possible.  Write a QuickCheck test
+-- which asserts that decompiling the output of your compiler
+-- results in the original expression, and vice versa.
+-- (Hint: It's not an error if the decompiler fails to decompile
+-- an instruction stream; just don't test the invariant in that case.)
+
+decompile :: [Instr] -> Maybe Expr
+decompile is =
+    case decompile' is [] of -- decompile' is defined in the utility
+        Just [e] -> Just e   -- section of this lab
+        _ -> Nothing
+
+prop_compile_decompile :: Expr -> Bool
+-- BEGIN prop_compile_decompile (DO NOT DELETE THIS LINE)
+prop_compile_decompile = undefined
+-- END prop_compile_decompile (DO NOT DELETE THIS LINE)
+
+prop_decompile_compile :: [Instr] -> Bool
+-- BEGIN prop_decompile_compile (DO NOT DELETE THIS LINE)
+prop_decompile_compile = undefined
+-- END prop_decompile_compile (DO NOT DELETE THIS LINE)
+
+-- That concludes this lab!  See the assigment release announcement
+-- on the class website for how to submit your lab.
+
+-------------------------------------------------------------------
+
+--                  Utility code
+
+-------------------------------------------------------------------
+
+-- This section contains some utility code for the rest of the lab.
+-- You may find it useful if you want to crib some syntax, or if
+-- you're just curious what's going on behind the scenes.  Reading
+-- this part is OPTIONAL.
+
+-- Our decompiler is implemented as a bit of abstract interpretation:
+-- instead of maintaining a stack of integers, we maintain a stack
+-- of expressions, and build them up as we execute instructions.
+
+decompile' :: [Instr] -> [Expr] -> Maybe [Expr]
+decompile' [] stk = Just stk
+decompile' (i:is) stk =
+    case decompileInstr i stk of
+        Nothing -> Nothing
+        Just stk' -> decompile' is stk'
+
+decompileInstr :: Instr -> [Expr] -> Maybe [Expr]
+decompileInstr (IPush n) stk = Just (Lit n:stk)
+decompileInstr (IOp b) (x:y:rest) = Just (Op b y x:rest)
+decompileInstr _ _ = Nothing
+
+-- The next section is devoted to generation of random instructions
+-- and expressions.  One interesting thing is that the random
+-- instruction generator operates by looking at the stack that would
+-- have been produced by the set of instructions generated so far, and
+-- only picking an instruction that would be valid in this state.
+-- If you are curious about this approach being used on a more
+-- interesting language, you may want to check out the paper
+-- "Testing Noninterference Quickly."
+--
+-- Something interesting about this code is that it is monadic.  We
+-- will be covering monads later in the quarter; you may find it
+-- interesting to revisit this code then.
+
+arbInstr :: Stack -> Gen Instr
+arbInstr (_:_:_) = oneof [IOp `fmap` arbitrary, IPush `fmap` choose (0,maxint)]
+arbInstr _ = IPush `fmap` choose (0,maxint)
+
+arbInstrs :: Int -> Stack -> [Instr] -> Gen [Instr]
+arbInstrs 0 [] rs = return (reverse rs)
+arbInstrs 0 stk rs = do
+    r <- IOp `fmap` arbitrary
+    case step r stk of
+        Nothing -> return (reverse rs)
+        Just stk' -> arbInstrs 0 stk' (r:rs)
+arbInstrs n stk rs = do
+    r <- arbInstr stk
+    case step r stk of
+        Nothing -> return rs
+        Just stk' -> arbInstrs (n-1) stk' (r:rs)
+
+--if __GLASGOW_HASKELL__ >= 708
+--instance {-# OVERLAPPING #-} Arbitrary [Instr] where
+--else
+--instance Arbitrary [Instr] where
+--endif
+--    arbitrary = sized $ \n -> do
+--                    k <- choose (1,n)
+  --                  arbInstrs k [] []
+
+instance Arbitrary Expr where
+    arbitrary = sized $ \n ->
+                    if n == 0
+                        then Lit `fmap` choose (1,maxint)
+                        else resize (n `div` 2)
+                            (frequency [(3, binop), (1, Lit `fmap` choose (0,maxint))])
+          where binop = do
+                    b <- arbitrary
+                    e1 <- arbitrary
+                    e2 <- arbitrary
+                    return (Op b e1 e2)
+
+    --shrink = expr_shrink
+
+instance Arbitrary BinOp where
+    arbitrary = oneof (map return [Plus, Minus, Times])
+
+instance Arbitrary Instr where
+    arbitrary = oneof ([IOp `fmap` arbitrary, IPush `fmap` choose (0,maxint)])
+
+-- We bound the integers generated from 0-10, just to make certain
+-- inputs more likely.
+
+maxint :: Int
+maxint = 10
+
+-- This is just like 'quickCheck', but testing more cases by default.
+
+check :: Testable prop => prop -> IO ()
+check = quickCheckWith stdArgs {maxSuccess = 1000}
